e31f081ed3ed9c26d144dc3d7521a39f3a17c225
denury-chowdhury <denuryc@gmail.com>
2024-09-23 06:47:16 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+          colors: {
+            primary:'#B4F461',
+            background: '#f9f7f3',
+            moneybg:'#f3f3f3',
+          },
+        fontFamilly: {
+             lexend: "Lexend",
+        }
+    }
+  },
+  plugins: [],
+}
