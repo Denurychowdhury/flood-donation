64fc4b55378c400f080eec858590b615c5c28d7c
denury-chowdhury <denuryc@gmail.com>
2024-09-24 12:47:54 +0600

    add comment on common function

--- js/common-function.js
+++ js/common-function.js
@@ -1,44 +1,51 @@
-
 // get element value and parse float
 function GetValueById(id) {
     const InputValue = parseFloat(document.getElementById(id).value);
     return InputValue;
 }
+// innertext with parse
 function GetAmountId(id) {
     const ammountValue =parseFloat(document.getElementById(id).innerText);
     return ammountValue;
 }
+
+// get innertext
 function GetTextId(id) {
     const ammountValue =document.getElementById(id).innerText;
     return ammountValue;
 }
+
+// add card amount
 function addAmount(ammountValue,inputValue) {
     const amount = ammountValue + inputValue;
     return amount;
 }
+// calculate total amount
 function getTotalamount(availableValu,inputValu) {
     const total = availableValu - inputValu;
     return total;
 }
-
+// get element by id
 function getId(id) {
     const identity = document.getElementById(id);
     return identity;
 }
 
-// const donationPage =getId('donation-page');
-// donationPage.classList.add('hidden');
-
+// add hidden class for button
 function addHidden(id) {
     const donation = document.getElementById(id)
     donation.classList.add('hidden');
     return;
 }
+// remove hidden class from button
 function removHidden(id) {
     const donation = document.getElementById(id)
     donation.classList.remove('hidden');
     return;
 }
+
+// remove value from input field after show modal
+
 function removeInput(id) {
     const remove = document.getElementById(id).value = '';
     return remove;
